154d036c79d3d8223dae503778939026eab8e8fa
Gabriel-Pacce <gabriel.pacce@unesp.br>
2024-08-20 16:46:34 -0300

    RodarMotor

---
+++ Paralelepipedo/Paralelepipedo.ino
@@ -0,0 +1,18 @@
+int motorA = 12;
+int motorB = 13;
+
+void setup() {
+  pinMode (motorA, OUTPUT);
+  pinMode (motorB, OUTPUT);
+
+
+
+}
+
+void loop() {
+  analogWrite (motorA, 255);
+  analogWrite (motorB, 0);
+  
+
+
+}
